d5dfc8afe8edd31857217300c1c9fb46855b9056
asavina2024 <avinashfeb94@gmail.com>
2024-09-13 11:49:37 +0530

    created new project

---
+++ src/test/java/org/sample/com/SampleGit.java
@@ -0,0 +1,16 @@
+package org.sample.com;
+
+public class SampleGit {
+	
+	private void Method1() {
+	System.out.println("I am method1");
+
+	}
+	
+	private void Main() {
+		SampleGit sg = new SampleGit();
+		sg.Method1();
+
+	}
+
+}
