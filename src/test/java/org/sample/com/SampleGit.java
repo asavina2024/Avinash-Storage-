947f99008083dfc1147cf0c9657c4fd585524645
asavina2024 <avinashfeb94@gmail.com>
2024-09-13 12:42:38 +0530

    modified

--- src/test/java/org/sample/com/SampleGit.java
+++ src/test/java/org/sample/com/SampleGit.java
@@ -7,6 +7,16 @@ public class SampleGit {
 
 	}
 	
+	private void mouse() {
+		System.out.println("Mouse");
+
+	}
+	
+	private void rat() {
+		System.out.println("rat");
+
+	}
+	
 	private void Main() {
 		SampleGit sg = new SampleGit();
 		sg.Method1();
